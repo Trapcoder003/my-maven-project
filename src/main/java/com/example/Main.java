2f5c8825525eb71748ed0457cde46d0b1d66a8d8
MR Nobody <blef7@localhost.localdomain>
2025-11-07 17:55:47 +0300

    Initial commit - Maven project

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import javax.swing.JFrame;
+
+public class Main {
+     public static void main(String[] args) {
+
+    JFrame frame;
+   
+   frame = new JFrame();
+   frame.setVisible(true);
+   frame.setSize(300,700);
+   
+
+       
+    }
+ 
+   
+}
