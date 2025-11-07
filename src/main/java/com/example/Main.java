81028b99872be9e4b52034fd10b575480b4d4071
MR Nobody <blef7@localhost.localdomain>
2025-11-07 18:22:31 +0300

    recent

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,18 +1,24 @@
 package com.example;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
      public static void main(String[] args) {
 
     JFrame frame;
+    JLabel label;
    
    frame = new JFrame();
    frame.setVisible(true);
    frame.setSize(300,700);
-   
+   frame.setLayout(null);
 
-       
+   label = new JLabel("hello");
+   label.setBounds(24, 67, 100, 466);
+   frame.add(label);
+    
+   
     }
  
    
